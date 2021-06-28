46539c600fae7bc806ca83dc496ebdc73c256f2b
Whiting <whitinghtennis@gmail.com>
2021-06-27 21:29:27 -0700

    commit tree af0573baefcb224bf10faf37a89aafc1a306def2

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance > 2500)
+  return 'No can do.';
+  else if (distance > 2000)
+  return 'I will gladly take your thirty bucks.';
+  else if (distance <= 400)
+  return 'This one is on me!';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+return city === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+/*
+MY FIRST TRY WITH ELSE IF:
+
+function ternaryCheckCity(city){
+if (city === 'NYC')
+return 'Ok, sounds good.';
+else return 'No go.';
+}
+*/
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous': 
+      return 'Thank you so much.';
+    break;
+    case 'not as generous':
+      return 'Thank you.';
+    break;
+    default:
+    return 'Bye.';
+  }
+   
 }
